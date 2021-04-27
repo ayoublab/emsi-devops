50c2753017472357c22387025eaa8efee10dc871
ayoublab <ayoublabhar1995@gmail.com>
2021-04-27 00:35:51 +0000

    modification on 1-install_nginx_web_server.sh in 0x02-web-server

--- 0x02-web_server/1-install_nginx_web_server.sh
+++ 0x02-web_server/1-install_nginx_web_server.sh
@@ -1,9 +1,8 @@
 #!/usr/bin/env bash
 
 sudo apt-get install nginx -y 
-mkdir /var/www/myHolbertonPage/
-cd /var/www/myHolbertonPage/
-touch index.html
-echo "Welcome to Holberton School" > index.html
-sudo sed - i "s/\/var\/www\/html/\/var\/www\/myHolberonPage" /etc/nginx/sites-enabled/default
+sudo mkdir /var/www/myHolbertonPage/
+sudo echo "Welcome to Holberton School" > index.html
+sudo cp index.html /var/www/myHolbertonPage/
+sudo sed -i s/"\/var\/www\/html"/"\/var\/www\/myHolbertonPage"/g /etc/nginx/sites-enabled/default
 sudo systemctl restart nginx 
